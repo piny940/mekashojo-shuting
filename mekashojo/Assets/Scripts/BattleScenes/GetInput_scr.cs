4b7efc48df75fc2ee02b553d255b83c48ad62656
mikan <s_ansai@icloud.com>
2021-06-01 19:04:58 +0900

    マウス座標の取得
    ・マウス座標の取得
    ・GetInputオブジェクトを全戦闘シーンにコピー

--- mekashojo/Assets/Scripts/BattleScenes/GetInput_scr.cs
+++ mekashojo/Assets/Scripts/BattleScenes/GetInput_scr.cs
@@ -10,7 +10,8 @@ public class GetInput_scr : MonoBehaviour
     [HideInInspector] public bool isMouseLeft;
     [HideInInspector] public bool isMouseRight;
     [HideInInspector] public bool isEscapeKey;
-    [HideInInspector] public Vector3 mousePosition;
+    [HideInInspector] public Vector3 mousePositionFromPlayer;   //プレイヤーからマウスへのベクトル
+    [SerializeField, Header("プレイヤーを入れる")] GameObject _player;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +33,25 @@ public class GetInput_scr : MonoBehaviour
         //右クリックの取得（押されている間ずっとtrue)
         isMouseRight = Input.GetMouseButton(1);
         //マウスカーソルの座標の取得
-        mousePosition = Input.mousePosition;    //ワールド座標に座標変換する必要がある！！
+        mousePositionFromPlayer = GetMousePositionFromPlayer(_player.transform.position);
         //エスケープキーの取得(押された瞬間のみtrue)
         isEscapeKey = Input.GetKeyDown(KeyCode.Escape);
-        
     }
+
+
+    /// <summary>
+    /// プレイヤーからマウスへのベクトルの取得
+    /// </summary>
+    /// <param name="_playerPosition"></param>
+    /// <returns></returns>
+    Vector3 GetMousePositionFromPlayer(Vector3 _playerPosition)
+    {
+        Vector3 _mousePositionOnScreen = Input.mousePosition;
+        Vector3 _mousePositionOnWorld = Camera.main.ScreenToWorldPoint(_mousePositionOnScreen);
+        Vector3 _mousePositionFromPlayer = _mousePositionOnWorld - _playerPosition;
+        _mousePositionFromPlayer.z = 0;
+        return _mousePositionFromPlayer;
+    }
+
+
 }
