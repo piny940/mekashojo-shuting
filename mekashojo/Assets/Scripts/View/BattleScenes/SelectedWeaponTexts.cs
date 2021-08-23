bb85021e36a89f2b39d4279ebc548796b75eea9d
mikan <s_ansai@icloud.com>
2021-08-23 16:12:45 +0900

    修正

--- mekashojo/Assets/Scripts/View/BattleScenes/SelectedWeaponTexts.cs
+++ mekashojo/Assets/Scripts/View/BattleScenes/SelectedWeaponTexts.cs
@@ -5,20 +5,10 @@ namespace View
 {
     public class SelectedWeaponTexts : MonoBehaviour
     {
-        // TODO: textとか名前つけてるけど実際は「Main」という文字が書かれた画像です。名前考えないといけない
-        [SerializeField, Header("MainTextを入れる")] private GameObject _mainText;
-        [SerializeField, Header("SubTextを入れる")] private GameObject _subText;
-
-        private Image _mainTextImage;
-        private Image _subTextImage;
+        [SerializeField, Header("MainTextImageを入れる")] private Image _mainTextImage;
+        [SerializeField, Header("SubTextImageを入れる")] private Image _subTextImage;
 
         private const float UNSELECTED_WEAPON_TRANSPARENCY = 0.2f;
-
-        private void Awake()
-        {
-            _mainTextImage = _mainText.GetComponent<Image>();
-            _subTextImage = _subText.GetComponent<Image>();
-        }
 
         // Start is called before the first frame update
         void Start()
