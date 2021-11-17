c5b96644adf9f5e8a2e5ca7ca210cefbb9464227
mikan <83708535+Ansai940@users.noreply.github.com>
2021-11-17 18:06:19 +0900

    バランス調整
    ステージの長さを1/4くらいに減らした

--- mekashojo/Assets/Scripts/View/BattleScenes/Player/PlayerModelManager.cs
+++ mekashojo/Assets/Scripts/View/BattleScenes/Player/PlayerModelManager.cs
@@ -22,7 +22,6 @@ namespace View
         {
             none,
             fire,
-            back,
             selectCannon,
             selectLaser,
             selectBeamMachineGun,
@@ -135,14 +134,6 @@ namespace View
         // サブ武器を発射する
         private void FireSubWeapon()
         {
-            // ミサイルの場合は、すでにアニメーションが始まっている場合を想定して、
-            // 一旦backトリガーをよぶ
-            if (Model.EquipmentData.equipmentData.selectedSubWeaponName
-                == Model.EquipmentData.equipmentType.SubWeapon__Missile)
-            {
-                _player.SetTrigger(animationParameters.back.ToString());
-                _subWeaponAnimator.SetTrigger(animationParameters.back.ToString());
-            }
             _player.SetTrigger(animationParameters.fire.ToString());
             _subWeaponAnimator.SetTrigger(animationParameters.fire.ToString());
         }
